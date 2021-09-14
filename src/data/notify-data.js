2d2957a5406a9d46b82cddc1366bdd5662517a4c
pranta-barua007 <prantabarua247@gmail.com>
2021-09-14 14:12:24 +0600

    updated repo url

--- src/data/notify-data.js
+++ src/data/notify-data.js
@@ -1,7 +1,7 @@
 export const notifyData = {
     name: 'Pranta Barua',
     email: 'prantabarua247@gmail.com',
-    repoUrl: 'testCheck',
+    repoUrl: 'https://github.com/pranta-barua007/meldcx-cool-test',
     token: window.localStorage.getItem('token'),
-    message: 'Hello Dear MeldCx, i want to take this moment to thank you for giving me the chance to prove myself. I always dreamed of having a team around me, where I can learn and grow also helping them with my dedication. I have tried to architect the flow of this app to best way possible (it will never be best i know :-)).Maybe I will have to spend some more time in styling and writing test for this app, continiously updating! Looking forward for your response'
+    message: 'Hello Dear MeldCX, i want to take this moment to thank you for giving me the chance to prove myself. I always dreamed of having a team around me, where I can learn and grow also helping them with my dedication. I have tried to architect the flow of this app to best way possible (it will never be best i know :-)).Maybe I will have to spend some more time in styling and writing test for this app, continiously updating! Looking forward for your response'
 };
