88465d35429e1912a872d0a9b912a0df9c517423
pranta-barua007 <prantabarua247@gmail.com>
2021-09-14 14:38:43 +0600

    message update in notify

--- src/data/notify-data.js
+++ src/data/notify-data.js
@@ -3,5 +3,28 @@ export const notifyData = {
     email: 'prantabarua247@gmail.com',
     repoUrl: 'https://github.com/pranta-barua007/meldcx-cool-test',
     token: window.localStorage.getItem('token'),
-    message: 'Hello Dear MeldCX, i want to take this moment to thank you for giving me the chance to prove myself. I always dreamed of having a team around me, where I can learn and grow also helping them with my dedication. I have tried to architect the flow of this app to best way possible (it will never be best i know :-)).Maybe I will have to spend some more time in styling and writing test for this app, continiously updating! Looking forward for your response'
+    message: `Hello Dear MeldCX, i want to take this moment to thank you for giving me the chance to prove myself.
+     I always dreamed of having a team around me, where I can learn and grow also helping them with my dedication.
+     Tried to architect the flow of this app to best way possible (it will never be best i know :-)).
+     Maybe I will have to spend some more time in styling and writing test for this app, but continiously updating!
+     Completed:
+        Component development and organisation
+        Interactions with an API
+        Attention to detail in design and implementation
+        Component and app styling
+        Ability to communicate
+    Ongoing: 
+        Effective testing
+    Features(completed): 
+        User enters any email address, and password 'meld123'
+        User clicks log in
+        User authenticated and taken to devices screen
+        User presented with up-to-date (polls every 5 seconds) view of active devices
+        User clicks 'notify' to signify completion of the test
+    Additionally(completed):
+        When user logs in, they should remain logged in until they click "log out", or their browser data is cleared
+        If password is incorrect, error message is shown
+        On the devices screen, the circles orbit around the number
+        On the devices screen, the number of circles shown should equal the number of active devices
+    Looking forward for your response`
 };
