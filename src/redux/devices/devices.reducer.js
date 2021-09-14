e2581e5b35c14fe484c262566e886f31734e5001
pranta-barua007 <prantabarua247@gmail.com>
2021-09-14 14:16:52 +0600

    updated repo url

--- src/redux/devices/devices.reducer.js
+++ src/redux/devices/devices.reducer.js
@@ -4,15 +4,11 @@ const INITIAL_STATE = {
     devicesData: [],
     isPending: false,
     error: null,
-    notify: false,
-    notifyMessage: '',
-    notifyErr: null
 };
 
 export const devicesReducer = (state=INITIAL_STATE, action) => {
     switch(action.type) {
         case deviceActionsTypes.FETCH_DEVICES_START:
-        case deviceActionsTypes.NOTIFY_START:
             return {
                 ...state,
                 isPending: true
@@ -28,19 +24,6 @@ export const devicesReducer = (state=INITIAL_STATE, action) => {
             return {
                 ...state,
                 error: action.payload,
-                isPending: false
-            }
-        case deviceActionsTypes.NOTIFY_SUCCESS:
-            return {
-                ...state,
-                notify: true,
-                notifyMessage: action.payload,
-                isPending: false
-            }
-        case deviceActionsTypes.NOTIFY_FAILED:
-            return {
-                ...state,
-                notifyErr: action.payload,
                 isPending: false
             }
         default: 
