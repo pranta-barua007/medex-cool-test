2ea3dd61325ee11792bd90dfffa4763642817316
pranta-barua007 <prantabarua247@gmail.com>
2021-09-24 11:24:38 +0600

    updated the ui of devices

--- src/components/devices/devices.component.jsx
+++ src/components/devices/devices.component.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react';
 import { Grid, Typography, Avatar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 
 import { connect } from 'react-redux';
 import { requestDevices } from '../../redux/devices/devices.actions';
@@ -15,29 +16,50 @@ function Devices({ devicesData, onDevicesMount }) {
     return () => clearInterval(interval); //cleanup while componenWilltUnmount
   }, [onDevicesMount]);
 
+  const useStyles = makeStyles(() => ({
+    topDevices: {
+      display: 'flex', 
+      flexDirection: 'row', 
+      justifyContent: 'space-between'
+    },
+    bottomDevices: {
+      display: 'flex', 
+      flexDirection: 'row-reverse', 
+      justifyContent: 'space-between'
+    },
+    avatar: {
+      backgroundColor: 'white',
+      padding: '10px', 
+      margin: '12px 0px', 
+      color: 'tomato'
+    }
+  }));
+
+  const classes = useStyles();
+
   return (
     <Grid container justifyContent='center' alignItems='center'>
-      <Grid item xs={12} style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+      <Grid item xs={12} sm={4} md={6} className={classes.topDevices}>
         {
           devicesData
             .filter((device, idx) => idx < Math.floor(devicesData.length/2))
-            .map(device => <Avatar key={device.id} style={{backgroundColor: 'white', padding: '10px', margin: '4px', color: 'tomato'}}>{device.name[0]}</Avatar>)
+            .map(device => <Avatar key={device.id} className={classes.avatar}>{device.name[0]}</Avatar>)
         }
       </Grid>
       <Grid item xs={12}>
-        <Typography variant='h1'>{devicesData.length}</Typography>
+        <Typography variant='h1' style={{fontFamily: 'Inter'}}>{devicesData.length}</Typography>
       </Grid>
       <Grid item xs={12}>
-        <Typography variant='subtitle1'> DEVICES</Typography>
+        <Typography variant='subtitle1' style={{fontWeight:'bold'}}>DEVICES</Typography>
       </Grid>
       <Grid item xs={12}>
-        <Typography variant='subtitle1'> ONLINE</Typography>
+        <Typography variant='subtitle1' style={{fontWeight:'bold'}}>ONLINE</Typography>
       </Grid>
-      <Grid item xs={12} style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+      <Grid item xs={12} sm={4} md={6} className={classes.bottomDevices}>
         {
           devicesData
             .filter((device, idx) => idx >= Math.floor(devicesData.length/2))
-            .map(device => <Avatar key={device.id} style={{backgroundColor: 'white', padding: '10px', margin: '4px', color: 'tomato'}}>{device.name[0]}</Avatar>)
+            .map(device => <Avatar key={device.id} className={classes.avatar}>{device.name[0]}</Avatar>)
         }
       </Grid>
     </Grid>
